6118fe33a13abd912d91a932e86ee33d86300669
Vitaly Bodnar <Lyl1.32@yandex.ru>
2024-10-06 21:37:07 +0300

    Translate to another language

---
+++ hello.pas
@@ -0,0 +1,19 @@
+﻿begin
+  Println('Введите язык, на котором вы говорите(ввод на английском)');
+  Println('For foreigners -> Enter the language you speak');
+  var hello := ReadString();
+  case hello of
+    'Russian':Print('Привет');
+    'English':Print('Hello');
+    'Portuguese':Print('Olá');
+    'Spanish':Print('Hola');
+    'Chinese':Print('你好');
+    'Japanese':Print('こんにちは');
+    'Korean':Print('안녕하세요');
+    'German':Print('Hallo');
+    'Indonesian':Print('Halo');
+    'Turkish':Print('Merhaba');
+    'Czech':Print('Ahoj');
+    'Indo-Aryan':Print('नमस्ते');
+  end;
+end.
